b471fb73d025ed137e83a431377f5c96a302cca6
Hugo Nascimento <hugovinitek@gmail.com>
2025-04-30 18:08:00 -0300

    editar o .env

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -5,12 +5,16 @@ import NavPages from "@/components/NavPages";
 import { Standard } from "@typebot.io/nextjs";
 
 export default function ChatbotPage() {
+    
+    const typebotId = process.env.TYPEBOT_ID;
+    const typebotApiHost = process.env.TYPEBOT_APIHOST;
+
     return(
         <div className="bg-[#d9d9d9]">
             <NavPages nome="Chatbot" />
             <Standard 
-                typebot="curabot-dyex2qv"
-                apiHost="https://typebot.io"
+                typebot={typebotId}
+                apiHost={typebotApiHost}
                 style={{ width: "100%", height: "650px", marginBottom: "8px" }}
             />
             <BottomNavigation />
