01e4e13a2fa74966a39865ee193f12f011897ec8
Hugo Nascimento <hugovinitek@gmail.com>
2025-06-20 10:33:38 -0300

    feat - chatbot

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -1,15 +1,54 @@
+'use client'
+
 import BottomNavigation from "@/components/BottomNavigation";
 import NavPages from "@/components/NavPages";
 import { Standard } from "@typebot.io/nextjs";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { supabase } from "@/utils/supabaseclient";
+import { useEffect, useState } from "react";
 
 export default function Chatbot() {
 
-    const typebotId = process.env.TYPEBOT_ID;
-    const typebotApiHost = process.env.TYPEBOT_APIHOST;
-    return(
+    const [user, setUser] = useState<any>(null);
+    const router = useRouter();
+    const pathname = usePathname();
+    const typebotId = process.env.NEXT_PUBLIC_TYPEBOT_ID;
+    const typebotApiHost = process.env.NEXT_PUBLIC_TYPEBOT_APIHOST;
+
+    useEffect(() => {
+        const fetchSession = async () => {
+            const { data: { session } } = await supabase.auth.getSession();
+            setUser(session?.user);
+            if (!session?.user) {
+                router.push('/auth/login');
+            }
+        };
+
+        fetchSession();
+
+        const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+            setUser(session?.user);
+            if (!session?.user) {
+                router.push('/auth/login');
+            }
+        });
+
+        return () => subscription?.unsubscribe();
+    }, [router]);
+
+    if (!user?.id) {
+        return <div>Carregando informações do usuário...</div>
+    }
+
+    const prefilledVariables = {
+        userId: user.id,
+    };
+
+    return (
         <div>
             <NavPages nome="Chatbot" />
-            <Standard 
+            <Standard
                 typebot={typebotId}
                 apiHost={typebotApiHost}
                 style={{ width: "100%", height: "600px" }}
